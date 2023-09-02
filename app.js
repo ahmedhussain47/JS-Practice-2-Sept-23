c33be023a14207d599b14dc3ad46aab47af0884b
Ahmed Hussain <68425053+ahmedhussain47@users.noreply.github.com>
2023-09-03 02:11:32 +0500

    JSPractice

---
+++ app.js
@@ -0,0 +1,22 @@
+// var nm1 = prompt("Enter name1: ").toUpperCase()
+// var nm2 = prompt("Enter name2: ").toLowerCase()
+// document.writeln(nm1+nm2)
+
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.writeln("<br/> Array ",fruits)
+document.writeln("<br/> Upper Case ",fruits[1].toUpperCase())
+document.writeln("<br/> Lower Case ",fruits[1].toLowerCase())
+document.writeln("<br/> Length",fruits.length)
+document.writeln("<br/> Length",fruits.length)
+fruits.pop()
+document.writeln("<br/> Pop: ",fruits)
+fruits.push("Ahmed","Raza","Hussain")
+document.writeln("<br/> push: ",fruits)
+fruits.shift("Ahmed")
+document.writeln("<br/> shift: ",fruits)
+fruits.unshift("Ahmed")
+document.writeln("<br/> unshift: ",fruits)
+fruits.splice(2,1,"Maheen")
+document.writeln("<br/> spice: ",fruits)
